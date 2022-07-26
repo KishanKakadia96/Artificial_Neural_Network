4345cfca79950e176d3e9661e92390893fe8c993
KishanKakadia96 <kishankakadia96@gmail.com>
2022-07-26 19:07:21 +0200

    First Commit

---
+++ Perceptron/utils/model.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self,eta,epochs):
+        self.weights = np.random.randn(3) * 1e-4  #small weight init
+        print(f"initial weights before training: {self.weights}")
+        self.eta = eta # Learning rate
+        self.epochs = epochs #one full cycle
+        
+    
+    def activationFunction(self,inputs,weights):
+        z = np.dot(inputs,weights) #z = W * X
+        return np.where(z > 0, 1, 0) #CONDITION, if TRUE, else FALSE
+        
+    
+    def fit(self, X, y):
+        self.X = X
+        self.y= y
+        
+        X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] #concatination 
+        print(f"X with bias: {X_with_bias}")
+        
+        for epoch in range(self.epochs):
+            print("--"*10)
+            print(f"for epoch: {epoch}")
+            print("--"*10)
+            
+            y_hat = self.activationFunction(X_with_bias,self.weights) #forward propagation
+            print(f"predicted value after forward pass: \n {y_hat}")
+            self.error = self.y - y_hat
+            print(f"Error: {self.error}")
+            
+            self.weights=self.weights + self.eta * np.dot(X_with_bias.T, self.error) #backword propagation
+            print(f"updated weights after epoch:\n{epoch}/{self.epochs} :\n {self.weights}")
+            print("#####"*10)
+                                
+                                             
+                                             
+                                             
+    def predict(self, X):
+        X_with_bias = np.c_[X, -np.ones((len(X), 1))]
+        return self.activationFunction(X_with_bias, self.weights)
+    
+    def total_loss(self):
+        total_loss = np.sum(self.error)
+        print(f"total loss: {total_loss}")
+        return total_loss
